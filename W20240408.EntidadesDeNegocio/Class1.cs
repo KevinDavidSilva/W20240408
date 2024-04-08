af91b6cf07a88c287b9a21164cb73f9aae2294b2
kevin david silva beltran <kevinsilvadb503@gmail.com>
2024-04-08 08:18:30 -0600

    la capa de entidades de negocios fue codificada con la clase PersonaW

--- W20240408.EntidadesDeNegocio/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace W20240408.EntidadesDeNegocio
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ W20240408.EntidadesDeNegocio/PersonaW.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W20240408.EntidadesDeNegocio
+{
+    public class PersonaW
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required(ErrorMessage ="El campo {0} es obligatorio")]
+        public string NombreW { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string ApellidoW { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public DateTime FechaNacimientoW { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public decimal SueldoW { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public byte  EstadusW { get; set; }
+        [Required(ErrorMessage = "El campo {0} es obligatorio")]
+        public string ComentarioW { get; set; }
+    }
+}
